ba3d87923281b45dba533f971fa2f6e855869a89
rohithsai18 <rohithsai18@gmail.com>
2019-04-10 08:35:28 +0530

    claim details added

--- src/pages/claim-details/claim-details.ts
+++ src/pages/claim-details/claim-details.ts
@@ -1,25 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { AccountServices } from '../../services/account-services';
 
-/**
- * Generated class for the ClaimDetailsPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
 @IonicPage()
 @Component({
   selector: 'page-claim-details',
   templateUrl: 'claim-details.html',
 })
-export class ClaimDetailsPage {
+export class ClaimDetailsPage implements OnInit{
+  claimDetails;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams,public accountServices:AccountServices) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ClaimDetailsPage');
+  }
+
+  ngOnInit(){
+     this.claimDetails = this.accountServices.getClaimsDetailsData();
+  }
+  logOut(){
+    this.navCtrl.popToRoot();
   }
 
 }
